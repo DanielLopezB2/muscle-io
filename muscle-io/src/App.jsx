f700597ee5941366bdcfdf77369ba4a602e55a14
Daniel Lopez <dalobe56@gmail.com>
2024-03-28 19:46:18 -0500

    Add font, bg and title

--- muscle-io/src/App.jsx
+++ muscle-io/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
   
   return (
-    <>
-      
-    </>
+    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <h1 className="text-center text-4xl font-bold text-white mt-10">
+        Muscle IO
+      </h1>
+
+      <p></p>
+    </div>
   )
 }
 
--- muscle-io/tailwind.config.js
+++ muscle-io/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Manrope Variable', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
