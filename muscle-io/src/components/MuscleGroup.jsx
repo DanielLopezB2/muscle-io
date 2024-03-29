32af65ba1d66bc17c872c8cc735363a16550fa83
Daniel Lopez <dalobe56@gmail.com>
2024-03-29 00:43:32 -0500

    Add links to muscle by id

--- muscle-io/src/components/MuscleGroup.jsx
+++ muscle-io/src/components/MuscleGroup.jsx
@@ -45,17 +45,17 @@ function MuscleGroup() {
                 <div key={index} className="p-4 hover:scale-105 transition ease-in-out">
 
                     <div className="w-72 bg-white border border-gray-200 rounded-lg shadow mt-12">
-                        <Link>
+                        <Link to={`/muscle/${item.id}`}>
                             <img className="rounded-t-lg" src={item.gifUrl} alt="" />
                         </Link>
 
                         <div className="p-5">
-                            <Link>
+                            <Link to={`/muscle/${item.id}`}>
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#160B34]">{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h5>
                             </Link>
 
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"><span className="font-semibold">Equipment: </span>{item.equipment}</p>
-                            <Link className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#160B34] rounded-lg hover:bg-[#5945b6] focus:ring-4 focus:outline-none transition ease-in-out">
+                            <Link to={`/muscle/${item.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#160B34] rounded-lg hover:bg-[#5945b6] focus:ring-4 focus:outline-none transition ease-in-out">
                                 Details
                                 <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
