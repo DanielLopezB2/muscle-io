35c8e3acc707088ad0c4396c072b245b7714b5cb
Daniel Lopez <dalobe56@gmail.com>
2024-03-28 20:11:33 -0500

    Add dark theme and Header component

--- muscle-io/src/App.jsx
+++ muscle-io/src/App.jsx
@@ -1,13 +1,12 @@
+import Header from "./components/Header"
 
 function App() {
   
   return (
-    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-      <h1 className="text-center text-4xl font-bold text-white mt-10">
-        Muscle IO
-      </h1>
+    <div className="absolute inset-0 -z-10 h-full w-full flex flex-col items-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
 
-      <p></p>
+      <Header />
+      
     </div>
   )
 }
---
+++ muscle-io/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div className="text-center max-w-4xl">
+            <h1 className="text-4xl font-semibold mt-16">Muscle IO</h1>
+
+            <p className="mt-10 text-lg"> <span className="font-semibold">Build</span> your dream physique with our muscle app. <span className="font-semibold">Achieve</span> your fitness goals faster than ever before. <span className="font-semibold">Discover </span>exercises to improve your training sessions.</p>
+        </div>
+
+    )
+}
